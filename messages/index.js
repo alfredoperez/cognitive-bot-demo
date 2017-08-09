cb1dde4320b5970abbe6c6e3ca29d68e88cae818
Alfredo Perez <alfredo.perez.q@gmail.com>
2017-08-09 06:49:00 -0500

    add code from default bot

--- messages/index.js
+++ messages/index.js
@@ -1,25 +1,40 @@
+"use strict";
 var restify = require('restify');
 var builder = require('botbuilder');
+var botbuilder_azure = require("botbuilder-azure");
+var path = require('path');
+
 var cognitiveservices = require('botbuilder-cognitiveservices');
+var useEmulator = (process.env.NODE_ENV == 'development');
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
-});
-
 // Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+var useEmulator = (process.env.NODE_ENV == 'development');
 
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+
+// Setup Restify Server
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function () {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    module.exports = {
+        default: connector.listen()
+    }
+}
 
 //=========================================================
 // Recognizers
